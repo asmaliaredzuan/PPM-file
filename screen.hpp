e3e74097441f73e45a0824ecec7879f8b0585523
asmaliaredzuan <asmaliaredzuan@gmail.com>
2021-02-15 17:57:25 -0600

    Update screen.hpp

--- screen.hpp
+++ screen.hpp
@@ -1,15 +1,3 @@
-/*
-* University of Arkansas at Little Rock
-* Bachelor in Computer Science
-* Asmalia Redzuan
-* T00666839
-* CPSC 3383: Programming Language
-* Assignment 1.2: Read/Write PPM image files
-* Description: 1. Using at least two distinct programming-language paradigm.
-*			   2. Write PPM image files with simple patterns and may be read and compare.
-* Date Written: November 30, 2020
-* Date Revised: December 5, 2020
-*/
 #pragma once
 
 #include <vector>
